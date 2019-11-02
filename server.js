c94886d541510f9e83bb74f73e2c5a9d3c95e459
ruthtech <leeruths@gmail.com>
2019-11-02 14:13:49 -0400

    Delete a note

--- server.js
+++ server.js
@@ -28,17 +28,16 @@ app.get("/notes", function(req, res) {
 
 // Read the db.json file and return all saved notes as JSON
 app.get("/api/notes", function(req, res) {
-    let statusCode = 200;
-    let notes = null;
     try {
+        res.status(200);
         res.json(dbAccess.getNotesJSON());
-    } 
+    }
     catch(err) {
         // Internal error on the server side.
-        statusCode = 500; 
+        res.status(500);
         res.json(err);
     }
-    return res.status(statusCode);
+    return res;
 });
 
 // Should recieve a query parameter containing the id of a note to delete. 
@@ -47,28 +46,37 @@ app.get("/api/notes", function(req, res) {
 // notes from the `db.json` file, remove the note with the given `id` 
 // property, and then rewrite the notes to the `db.json` file.
 app.delete("/api/notes/:id", function(req, res) {
-    const success = dbAccess.deleteNote(req.params.id);
+    try {
+        const success = dbAccess.deleteNote(req.params.id);
 
-    if(success) {
-        res.status(204); // HTML 204 request succeeded
-    } else {
-        res.status(404); // HTML status 404 not found
+        if(success) {
+            res.status(204); // HTML 204 request succeeded
+        } else {
+            res.status(404); // HTML status 404 not found
+        }
+        res.json(dbAccess.getNotesJSON());
     }
+    catch(err) {
+        // Internal error on the server side.
+        res.status(500);
+        res.json(err);
+    }
+    return res;
 });
 
 // Receive a new note to save on the request body, add it to
 // the db.json file, and then return the new note to the client.
 app.post("/api/notes", function(req, res) {
-    let statusCode = 201; // HTML status 201 creation successful
     try {
+        res.status(201); // HTML status 201 creation successful
         res.json(dbAccess.addNote(req.body));
     }
     catch(err) {
         // Internal error on the server side.
-        statusCode = 500; 
+        res.status(500);
         res.json(err);
     }
-    return res.status(statusCode); 
+    return res; 
 });
 
 // Starts the server to begin listening
